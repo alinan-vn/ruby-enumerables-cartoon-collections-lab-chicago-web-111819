be3952684c0df5eacdc4b9cf28139e12e6126205
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 15:30:57 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,4 +16,4 @@ def find_the_cheese# code an argument here
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
-puts roll_call_dwarves(["first", "second", "thirds"])
+puts roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
