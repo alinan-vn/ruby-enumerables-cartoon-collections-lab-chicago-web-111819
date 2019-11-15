9d08ec1ac9bfbde18a34c511c763002d7ceaff72
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 15:48:53 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,9 @@ def roll_call_dwarves(array)
   puts call 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  roll = array.map {|v| "#{v.capitalize}!" }
+  return roll 
 end
 
 def long_planeteer_calls# code an argument here
