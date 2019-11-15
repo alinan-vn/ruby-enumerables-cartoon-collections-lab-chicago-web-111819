ff5a321949ce6bd43415ba86b4c8a65a2bd6350a
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 15:54:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  value = array.any? {|v| v.length >= 4 }
+  value = array.any? {|v| v.length > 4 }
   return value 
 end
 
