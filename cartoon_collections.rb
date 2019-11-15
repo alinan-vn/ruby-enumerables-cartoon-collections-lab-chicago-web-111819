b8f540b77efeed747994e6c2ec7913c24de89999
Alejandro Linan <jandrolinan@gmail.com>
2019-11-15 15:50:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,13 +10,11 @@ def summon_captain_planet(array)
   return roll 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-
-puts roll_call_dwarves(["Doc", "Dopey", "Bashful", "Grumpy"])
